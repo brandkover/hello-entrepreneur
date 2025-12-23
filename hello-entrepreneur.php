09d6b01d47d4d1f30f49029ffa17266902ea790d
Uber Chuquimia <141504719+ubergabriel@users.noreply.github.com>
2025-12-23 09:03:37 -0400

    Rename plugin and text domain to 'Hello Entrepreneur'

--- hello-entrepreneur.php
+++ hello-entrepreneur.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name: Hola Emprendedor
+ * Plugin Name: Hello Entrepreneur
  * Plugin URI:  https://github.com/brandkover/hola-emprendedor/
  * Description: A simple and inspiring add-on for entrepreneurs. Displays a random motivational quote at the top of your admin panel.
  * Version:     1.1.0
  * Author:      Brandkover
  * Author URI:  https://brandkover.com
  * License:     GPL v2 or later
- * Text Domain: hello-entrepeneur
+ * Text Domain: hello-entrepreneur
  */
 
 namespace HolaEmprendedor;
